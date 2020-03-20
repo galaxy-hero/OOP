38d8a496452150e936f3fdc88f88c233756041d6
ArseneAndreea <44430248+ArseneAndreea@users.noreply.github.com>
2020-03-20 12:57:56 +0200

    Lab 5 initial commit

---
+++ Laborator 5/Project1/Project1/Number.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Number
+{
+	// add data members
+public:
+	Number(const char * value, int base); // where base is between 2 and 16
+	~Number();
+
+	// add operators and copy/move constructor
+
+	void SwitchBase(int newBase);
+	void Print();
+	int  GetDigitsCount(); // returns the number of digits for the current number
+	int  GetBase(); // returns the current base
+};
